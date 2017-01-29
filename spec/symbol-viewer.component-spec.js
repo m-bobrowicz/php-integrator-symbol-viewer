9b3625a63b65a1f681fa29b8b9f68a83baa63cd6
Michal Bobrowicz <bobrowicz.michal@gmail.com>
2017-01-29 23:51:29 +0100

    WIP

--- spec/symbol-viewer.component-spec.js
+++ spec/symbol-viewer.component-spec.js
@@ -8,31 +8,11 @@ const SymbolViewerComponent = require('../lib/symbol-viewer.component');
 
 describe('symbolViewerComponent', function() {
   beforeEach(function() {
-    SymbolViewerComponent.store = createStore(function(state, action) {
-      if (action.type === 'togglePanelCollapsed') {
-        return { collapsed: !state.collapsed };
-      }
-      return state;
-    }, {
-      collapsed: false,
-    });
-    
-    SymbolViewerComponent.togglePanel = function() {
-      SymbolViewerComponent.store.dispatch({
-        type: 'togglePanelCollapsed',
-      });
-    };
-
     this.container = document.createElement('atom-php-integrator-symbol-viewer');
-
-    this.render = function() {
-        ReactDOM.render(h(SymbolViewerComponent), this.container);
-    };
-
-    this.render();
   });
 
   it('should be collapsed by default', function() {
+    ReactDOM.render(h(SymbolViewerComponent), this.container);
     const symbolViewer = this.container.querySelector('.symbol-viewer');
 
     expect(symbolViewer).toExist();
@@ -40,10 +20,11 @@ describe('symbolViewerComponent', function() {
   })
 
   it('should toggle panel on button click', function() {
+    ReactDOM.render(h(SymbolViewerComponent), this.container);
     const toggleButton = this.container.querySelector('.toggle-button-container');
     const symbolViewer = this.container.querySelector('.symbol-viewer');
-    Simulate.click(toggleButton);
-    this.render();
+    Simulate.click(toggleButton);    
+    ReactDOM.render(h(SymbolViewerComponent), this.container);
 
     expect(symbolViewer).toExist();
     expect(symbolViewer).toHaveClass('symbol-viewer--collapsed');
